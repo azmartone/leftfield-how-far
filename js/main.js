e17bb2ae75405dc7b27cd754ee141a0264990f30
Danny Duong <dannyduong888@gmail.com>
2013-12-18 15:37:51 -0800

    Coffe cups visualized

--- js/main.js
+++ js/main.js
@@ -3,9 +3,6 @@ function DistanceCalculator(data, callback) {
   	init: function(){
   		var origins = this.accessDataLayer("origins");
   		var destinations = this.accessDataLayer("destinations");
-  		
-  		console.log(origins);
-  		console.log(destinations);
 
   		this.calculateDistances(origins, destinations);
   	},
@@ -31,14 +28,13 @@ function DistanceCalculator(data, callback) {
 		    origins: origins,
 		    destinations: destinations,
 		    travelMode: google.maps.TravelMode.DRIVING,
-		    unitSystem: google.maps.UnitSystem.IMPERIAL,
+		    unitSystem: google.maps.UnitSystem.METRIC,
 		    avoidHighways: false,
 		    avoidTolls: false
 		  }, this.serviceCallback);
 		},
 
 		serviceCallback : function(result){
-			// console.log("result", result);
 			callback(result);
 		}
 	}
@@ -49,27 +45,70 @@ function DistanceCalculator(data, callback) {
 $(document).ready(function(){
 	var distanceCalculator = new DistanceCalculator(geodata, callback);
 	distanceCalculator.init();
+	renderDestinations(geodata.destinations);
 });
 
 
 var callback = function(result){
+	var cupInMeters = 0.1016;
+	var maxDistance = 0;
 	//Add distances to the data object
 	var distances = result.rows[0].elements;
 	$.each(distances, function(index, value){
-		geodata.destinations[index].distance = value.distance;
+		if (value.distance != null){
+			distance = value.distance;
+
+			maxDistance = distance.value > maxDistance ? distance.value : maxDistance;
+			distance.cups = parseInt(distance.value / cupInMeters);
+			geodata.destinations[index].distance = distance;
+		}
 		// geodata[index].distance = value;
 	});  
 	sortedDestinations = geodata.destinations.sort(sortDestinationByDistanceValue);
+
+	renderDestinations(sortedDestinations, maxDistance);
 }
 
 //Custom Sort Function
 var sortDestinationByDistanceValue = function (a,b){  
-	console.log("a", a, "b", b);
 	if (a.distance == null)
 		return 1;
 	if (b.distance == null)
 		return -1;
   return a.distance.value > b.distance.value ? 1 : -1;  
+};
+
+
+//On destination add
+//Find the destination distance
+//Add destination to the collection
+//Re-Sort the collection
+//Re-render the visualization
+var renderDestinations = function(destinations, maxDistance){
+
+	console.log(destinations);
+	var innerHTML = '';
+	var percentage;
+
+	$.each(destinations, function(index, value){
+		percentage = (value.distance == null)? 0 : (value.distance.value / maxDistance * 100);
+		percentage = percentage + "%";
+
+		console.log("percentage", percentage);
+		
+		innerHTML += '<div class="destination">' +
+				            '<div class="description">'+
+				                '<span class="search">' + value.description + '</span>' +
+				                '<span class="amount"></span>' +
+				            '</div>' +
+				            '<div class="bar" style="width:'+ percentage +'"></div>' +
+				        '</div>';
+	});
+
+
+	$(".destinations-container").html(innerHTML);
+	// console.log("innerHTML", innerHTML);
+
 };
 
 
